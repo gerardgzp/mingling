96047846b53be1abae363c57fa6a52ef0e072540
张翠莹 <zhangcuiying@skyense.com>
2025-02-07 18:02:12 +0800

    pandas_study2

--- study/pd_study.py
+++ study/pd_study.py
@@ -1,4 +1,49 @@
 import pandas as pd
-csv_path=r"D:\aguo\数据集\临时\archive\train_and_test2.csv"
+import numpy as np
+csv_path=r"D:\aguo\数据集\临时\archive\Titanic-Dataset.csv"
 df=pd.read_csv(csv_path)
-print(df)
+
+
+bool_series=pd.isnull(df['Age'])
+# print(bool_series)
+missing_gender_data = df[bool_series]
+print(missing_gender_data)
+
+
+
+
+# survived_cl=df['Survived']
+# total=len(survived_cl)
+# # print(total)
+# counts=survived_cl.value_counts()
+# # print(counts)
+# n_0=counts.get(0)
+# n_1=counts.get(1)
+# # print(n_1)
+# # print(n_0)
+# survival_rate=n_1/total
+# # print(survival_rate)
+# p_0="{:.2f}".format(n_0/total)
+# # print(p_0)
+#
+#
+# sex_counts=df['Sex'].value_counts()
+# male=sex_counts.get('male')
+# female=sex_counts.get('female')
+# sex_total=len(df['Sex'])
+# # print(f"{sex_total} {male} {female} {sex_counts}")
+# # print(type(df))
+#
+# data = {'Category': ['A', 'B', 'A', 'B', 'A'],'Value': [10, 20, 15, 25, 30]}
+# df1=pd.DataFrame(data)
+# grouped=df1.groupby(by='Category')
+# print(grouped)
+# res1 = grouped.mean()
+# print(res1)
+# data = {'First Score': [100, 90, np.nan, 95],
+#         'Second Score': [30, 45, 56, np.nan],
+#         'Third Score': [np.nan, 40, 80, 98]}
+#
+# df = pd.DataFrame(data)
+# missing_values = df.isnull()
+# print(missing_values)
